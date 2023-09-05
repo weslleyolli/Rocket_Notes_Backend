5be7164c79bc760d2a673c7b8df7f499f8a99b25
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-05 15:51:12 -0300

    updating users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,5 +8,6 @@ const usersRoutes = Router()
 const usersControllers = new UsersControllers()
 
 usersRoutes.post("/", usersControllers.create)
+usersRoutes.put("/:id", usersControllers.update)
 
 module.exports = usersRoutes
