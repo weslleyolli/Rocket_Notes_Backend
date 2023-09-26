5672f0ab0d3f283cd3b6aede502c201aaab79087
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-26 18:27:24 -0300

    loading imagens

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,15 +1,21 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UsersControllers = require("../controllers/usersControllers")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router()
-
+const upload = multer(uploadConfig.MULTER)
 
 const usersControllers = new UsersControllers()
 
 usersRoutes.post("/", usersControllers.create)
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update)
 usersRoutes.delete("/:id", usersControllers.delete)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    console.log(request.file.filename)
+    response.json()
+})
 
 module.exports = usersRoutes
